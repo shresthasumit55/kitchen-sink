b00b43820844c3c59311c5829ddfe446fd8c1f56
PratisthaS <pratisthashres@gmail.com>
2019-10-16 19:32:22 -0500

    Update MemberRegistrationIT.java

--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationIT.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/MemberRegistrationIT.java
@@ -35,15 +35,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Arquillian.class)
-public class MemberRegistrationIT {
+public class MemberRegistrationTest {
     @Deployment
     public static Archive<?> createTestArchive() {
         return ShrinkWrap.create(WebArchive.class, "test.war")
-            .addClasses(Member.class, MemberRegistration.class, Resources.class)
+            .addClasses(Member.class, MemberRegistration.class, Resources.class, Logger.class)
             .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
             .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
             // Deploy our test datasource
             .addAsWebInfResource("test-ds.xml");
+           
     }
 
     @Inject
