24f6a97c547edc6876b9a3d5f0235cc4eee27d48
shresthasumit55 <shresthasumit55@gmail.com>
2019-10-17 11:22:04 -0500

    Update ContestService.java

--- src/main/java/org/jboss/as/quickstarts/kitchensink/service/ContestService.java
+++ src/main/java/org/jboss/as/quickstarts/kitchensink/service/ContestService.java
@@ -1,10 +1,13 @@
 package org.jboss.as.quickstarts.kitchensink.service;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 import org.jboss.as.quickstarts.kitchensink.data.ContestRepository;
 import org.jboss.as.quickstarts.kitchensink.model.Contest;
+import org.jboss.as.quickstarts.kitchensink.model.Team;
 
 @Stateless
 public class ContestService {
@@ -17,6 +20,30 @@ public class ContestService {
 	}
 	
 	public void update(Contest contest) {
-		contestRepository.save(contest);
+		if (contest.getRegistration_allowed())
+			contestRepository.save(contest);
+	}
+	
+	public Contest getContestById(Long id) {
+		return contestRepository.findBy(id);
+	}
+	
+	public List<Contest> getAllContests(){
+		return contestRepository.findAll();
+	}
+	
+	public boolean isContestFull(Contest contest) {
+		Integer contestCapacity = contest.getCapacity();
+		Integer contestSize = contest.getTeamSet().size();
+		if (contestSize<contestCapacity) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean isContestWritable(Contest contest) {
+		if (contest.getRegistration_allowed())
+			return true;
+		return false;
 	}
 }
